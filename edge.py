38c37efaabca6daff0caf286db03f80fab95b29a
iamernie8199 <iamernie8199@gmail.com>
2024-06-24 15:52:02 +0800

    Update edge.py

--- edge.py
+++ edge.py
@@ -1,3 +1,4 @@
+# %%
 import os
 import pandas as pd
 from time import sleep
@@ -5,35 +6,67 @@ from multiprocessing import Pool, cpu_count
 from glob import glob
 
 from tqdm import tqdm
+import numpy as np
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 
-options = Options()
-appState = {
-    'recentDestinations': [{
-        'id': 'Save as PDF',
-        'origin': 'local',
-        'account': ''
-    }],
-    'selectedDestinationId': 'Save as PDF',
-    'version': 2
-}
-prefs = {
-    "download.default_directory": os.path.join(os.getcwd(), 'tmp'),
-}
-options.add_experimental_option('prefs', prefs)
-options.add_argument('--kiosk-printing')
+def file_process(path):
+    idx = pd.read_excel(path)
+    try:
+        idx = idx[idx['Unnamed: 2'].str.replace(' ', '') == '公司名稱'].index[0]
+        return pd.read_excel(path, header=idx+1)
+    except:
+        print(path)
 
 
+def data_process(dataframe):
+    try:
+        dataframe.columns = dataframe.columns.str.replace('\n', '')
+        dataframe.columns = dataframe.columns.str.replace('　　', '')
+        dataframe.columns = dataframe.columns.str.replace(' ', '')
+    except:
+        return
+    if 'Unnamed:13' in dataframe.columns:
+        dataframe = dataframe.drop(columns=['Unnamed:13'])
+    
+    if dataframe['公司型態'].dtype == 'int64':
+        dataframe['公司型態'] = dataframe['公司型態'].map({1: '上市', 
+                                                        2: '上櫃', 
+                                                        3: '未上市(櫃)',
+                                                        4: '補辦公開發行',
+                                                        5: '興櫃',
+                                                        6: '外國企業'})
+    dataframe[['承銷商', '發行價格']] = dataframe[['承銷商', '發行價格']].replace(' ', np.nan)
+    return dataframe.rename(columns={'金額(元)': '金額',
+                                     '申報生效日期': '生效日期',
+                                     '備註1': '案件性質',
+                                     '申報事項': '案件類別'})
+
+
+# %%
 if __name__ == '__main__':
     if not os.path.exists('tmp'):
         os.makedirs('tmp')
+
+    options = Options()
+    appState = {
+        'recentDestinations': [{
+            'id': 'Save as PDF',
+            'origin': 'local',
+            'account': ''
+        }],
+        'selectedDestinationId': 'Save as PDF',
+        'version': 2
+    }
+    prefs = {
+        "download.default_directory": os.path.join(os.getcwd(), 'tmp'),
+    }
+    options.add_experimental_option('prefs', prefs)
+    options.add_argument('--kiosk-printing')
 
     driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()), options=options)
     driver.get(f"https://www.sfb.gov.tw/ch/home.jsp?id=95&parentpath=0,2")
@@ -53,3 +86,17 @@ if __name__ == '__main__':
             driver.get(driver.find_element('xpath', "//a[contains(@title, '生效案件.xls')]").get_attribute('href'))
         except:
             pass
+    # %%
+    file_list = glob('tmp/預計*')
+
+    p = Pool(int(cpu_count() * 0.8))
+    result = list(tqdm(p.imap(file_process, file_list), total=len(file_list)))
+    p.close()
+    p.join()
+    
+    p = Pool(int(cpu_count() * 0.8))
+    result2 = list(tqdm(p.imap(data_process, result), total=len(result)))
+    p.close()
+    p.join()
+    
+    result = result2
